6e541333a734c9c738c627b2e57fc246545e0661
Ethan Estrada <ethan@misterfidget.com>
2026-05-08 17:51:31 -0500

    Add more docs for mixin methods

--- lib/asynchronous_futures.rb
+++ lib/asynchronous_futures.rb
@@ -11,6 +11,8 @@ module AsynchronousFutures
 
   class Error < StandardError; end
 
+  class ConcurrencyUnavailable < Error; end
+
   # Class for async execution results.
   class Future # rubocop:disable Metrics/ClassLength
     include MonitorMixin
@@ -227,27 +229,31 @@ module AsynchronousFutures
     end
   end
 
-  # Base Executor class. Has a simple implementation that just runs submitted
-  # functions immediately and returns a completed Future.
+  # Executor mixin module. Has a simple implementation that just runs submitted
+  # functions immediately and returns a completed Future. Can be used standalone
+  # as a stateless Executor that runs submitted blocks immediately.
   #
-  # Inheriting classes must override the `submit` method.
-  class Executor
-    def initialize(*_args, **_kwargs)
-      if block_given? # rubocop:disable Style/GuardClause
-        begin
-          yield self
-        ensure
-          shutdown
-        end
-      end
-    end
+  # Classes using this mixin should override the `submit` method.
+  #
+  # `shutdown` should be overridden if there is cleanup to be performed.
+  #
+  # If an implementation wants to signal that it supports true concurrency, it
+  # should override `submit_concurrent`; this can be as simple as aliasing it to
+  # the previously overridden `submit` method.
+  #
+  # The `map` method should *never* be overridden. This is already logically
+  # correct and should work with any Executor implementation.
+  module Executor
+    # Schedules the block, to be executed as `block.call(*args, **kwargs)` and
+    # returns a `Future` object representing the execution of the block.
+    def submit(*args, **kwargs, &block) # rubocop:disable Style/ArgumentsForwarding
+      raise ArgumentError.new('No block given') unless block
 
-    def submit(fun, *args, **kwargs, &) # rubocop:disable Style/ArgumentsForwarding
       future = Future.new
       future.set_running_or_notify_cancel
 
       begin
-        result = fun.call(*args, **kwargs, &) # rubocop:disable Style/ArgumentsForwarding
+        result = block.call(*args, **kwargs) # rubocop:disable Style/ArgumentsForwarding
       rescue Exception => e # rubocop:disable Lint/RescueException
         future.set_exception(e)
       else
@@ -257,25 +263,96 @@ module AsynchronousFutures
       future
     end
 
-    def map(fun, *iterables, timeout_sec: nil) # rubocop:disable Lint/UnusedMethodArgument
-      fs = iterables.map { |args| submit(fun, *args) }
+    # Schedules the block, to be executed as `block.call(*args, **kwargs)` and
+    # returns a `Future` object representing the execution of the block.
+    #
+    # Executor must support concurrency otherwise this method will raise the
+    # exception `ConcurrencyUnavailable`.
+    def submit_concurrent(*_args, **_kwargs, &)
+      raise ConcurrencyUnavailable
+    end
+
+    # Similar to map(fn, *enumerator) except:
+    #
+    # the enumerator are collected immediately rather than lazily;
+    #
+    # fn is executed asynchronously and several calls to fn may be made
+    # concurrently.
+    #
+    # The returned iterator raises a TimeoutError if __next__() is called and
+    # the result isn’t available after timeout seconds from the original call to
+    # Executor.map(). timeout can be an int or a float. If timeout is not
+    # specified or None, there is no limit to the wait time.
+    #
+    # If a fn call raises an exception, then that exception will be raised when
+    # its value is retrieved from the iterator.
+    #
+    # When using ProcessPoolExecutor, this method chops enumerator into a number
+    # of chunks which it submits to the pool as separate tasks. The
+    # (approximate) size of these chunks can be specified by setting chunksize
+    # to a positive integer. For very long enumerator, using a large value for
+    # chunksize can significantly improve performance compared to the default
+    # size of 1. With ThreadPoolExecutor, chunksize has no effect.
+    def map(*enumerator, timeout_sec: nil, &block) # rubocop:disable Lint/UnusedMethodArgument
+      # Use `to_a` in case the enumerator is lazy (we *want* to be eager in this
+      # circumstance).
+      futures = enumerator.map { |args| submit(args, &block) }.to_a
 
       # FIXME: Need to implement this as an internal enumerator or something so
-      # that cleanup can be assured. For example, there needs to be an ensure
-      # section to attempt to cancel all futures.
+      # that cleanup can be assured and we can support timeouts. For example,
+      # there needs to be an ensure section to attempt to cancel all futures.
       #
       # See: https://docs.ruby-lang.org/en/3.3/Enumerator.html#class-Enumerator-label-Convert+External+Iteration+to+Internal+Iteration
       # See: https://github.com/python/cpython/blob/59b260c61b5abb75edcb2b0ab901274a58dfc856/Lib/concurrent/futures/_base.py#L612-L625
       # See: https://docs.ruby-lang.org/en/3.3/Enumerable.html#method-i-zip
-      fs.lazy.map do |f|
+      futures.lazy.map do |f|
         f.result
       ensure
         f.cancel
       end
     end
 
-    def shutdown(wait = True, cancel_futures = False)
+    # Signal the executor that it should free any resources that it is using
+    # when the currently pending futures are done executing. Calls to
+    # Executor.submit() and Executor.map() made after shutdown will raise
+    # RuntimeError.
+    #
+    # If wait is True then this method will not return until all the pending
+    # futures are done executing and the resources associated with the executor
+    # have been freed. If wait is False then this method will return immediately
+    # and the resources associated with the executor will be freed when all
+    # pending futures are done executing. Regardless of the value of wait, the
+    # entire Python program will not exit until all pending futures are done
+    # executing.
+    #
+    # If cancel_futures is True, this method will cancel all pending futures
+    # that the executor has not started running. Any futures that are completed
+    # or running won’t be cancelled, regardless of the value of cancel_futures.
+    #
+    # If both cancel_futures and wait are True, all futures that the executor
+    # has started running will be completed prior to this method returning. The
+    # remaining futures are cancelled.
+    #
+    # You can ensure this gets called under all circumstances by calling this
+    # method with a block. The block will be called and then any shutdown
+    # cleanup logic will be run after the block completes. The block will be
+    # passed one parameter: the executor instance.
+    #
+    # ThreadExecutor.new(max_workers=4).shutdown do |e|
+    #     e.submit('src1.txt', 'dest1.txt', &FileUtils.cp)
+    #     e.submit('src2.txt', 'dest2.txt', &FileUtils.cp)
+    #     e.submit('src3.txt', 'dest3.txt', &FileUtils.cp)
+    #     e.submit('src4.txt', 'dest4.txt', &FileUtils.cp)
+    # end
+    def shutdown(wait = True, cancel_futures = False, &block) # rubocop:disable Lint/UnusedMethodArgument
       # In the base implementation, there is nothing to cleanup.
+      block&.call(self)
+    ensure # rubocop:disable Lint/EmptyEnsure
+      # Cleanup logic goes here
+      #
+      # The mixin has no state, so it has nothing to cleanup.
     end
+
+    module_function :submit, :submit_concurrent, :map, :shutdown
   end
 end
