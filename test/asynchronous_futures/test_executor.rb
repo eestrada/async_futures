093dd41844307572219b6b5a0a76e5111294879f
Ethan Estrada <ethan@misterfidget.com>
2026-05-10 22:53:53 -0500

    Add new test file
    Rename `test_helper` to `minitest_helper` to avoid these files being
    picked up as if they were actual test suites.

--- test/asynchronous_futures/test_executor.rb
+++ test/asynchronous_futures/test_executor.rb
@@ -2,13 +2,9 @@
 
 require_relative 'minitest_helper'
 
-require 'asynchronous_futures'
+require 'asynchronous_futures/executor'
 
-class TestAsynchronousFutures < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::AsynchronousFutures::VERSION
-  end
-
+class TestExecutor < Minitest::Test
   def test_it_does_something_useful
     skip 'nothing to test yet'
 
