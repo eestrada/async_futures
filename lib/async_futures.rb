d586c07032331ad4d4810aee8651870a1d784cbe
Ethan Estrada <ethan@misterfidget.com>
2026-05-24 15:23:04 -0500

    Require version 4.x of Ruby for RactorExecutor

--- lib/async_futures.rb
+++ lib/async_futures.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+# Library to create Future instances.
+#
+# Has Executor implementations for Ractor, Thread, and Fiber primitives.
+module AsyncFutures
+end
+
 # In order of dependency (roughly)
 require_relative 'async_futures/logger'
 require_relative 'async_futures/version'
@@ -7,11 +13,8 @@ require_relative 'async_futures/error'
 require_relative 'async_futures/future'
 require_relative 'async_futures/executor'
 require_relative 'async_futures/fiber_executor'
-require_relative 'async_futures/ractor_executor'
 require_relative 'async_futures/thread_executor'
 
-# Library to create Future instances.
-#
-# Has Executor implementations for Ractor, Thread, and Fiber primitives.
-module AsyncFutures
-end
+# ractor executor is only support in version 4.x or greater,
+# so it must be required explicitly.
+# require_relative 'async_futures/ractor_executor'
--- lib/async_futures/ractor_executor.rb
+++ lib/async_futures/ractor_executor.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
+# The Ractor API was different before version 4.x of Ruby.
+unless /^4\./ === RUBY_VERSION
+  raise LoadError.new("'async_futures/ractor_executor' is not supported in Ruby version '#{RUBY_VERSION}'")
+end
+
 require_relative 'executor'
 
 module AsyncFutures
+  # `Executor` implementation based on `Ractor` primitives
+  # that uses a pool of up to `max_workers` to execute calls concurrently.
+  #
+  # Only available in Ruby version 4.0 and higher.
+  # Requiring this file in earlier versions will raise a `LoadError`.
   class RactorExecutor
     include Executor
   end
