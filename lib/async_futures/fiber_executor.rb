76343248914e50df24ecc4f87c55e7fc2fbcc0fb
Ethan Estrada <ethan@misterfidget.com>
2026-06-15 16:31:01 -0600

    Revert "Remove FiberExecutor"
    This reverts commit ae23f6b90a3a1bda27f7f691dd95ab235c2704bd.

---
+++ lib/async_futures/fiber_executor.rb
@@ -0,0 +1,148 @@
+# frozen_string_literal: true
+
+require_relative 'error'
+require_relative 'executor'
+
+require 'set' # rubocop:disable Lint/RedundantRequireStatement
+
+module AsyncFutures
+  # `Executor` implementation based on `Fiber` primitives.
+  # Requires that `Fiber.scheduler` be set in order to work.
+  #
+  # Several benefits of using `FiberExecutor` over using `Fiber.schedule` directly:
+  #
+  # - By default `Fiber` instances run via `Fiber.schedule`
+  #   have no straightforward way of returning their final result
+  #   upon completion
+  #   like `Thread` and `Ractor` do
+  #   (both support calling `value` to get the final result).
+  #   `Future` makes this trivial to do for a scheduled `Fiber`.
+  # - `Fiber` instances cannot currently be shared across `Thread` instances
+  #   (though this may change someday).
+  #   However, `Future` instances can safely be shared
+  #   across both `Threads` and `Fibers`
+  #   (Ractors can share neither `Fiber` nor `Future`
+  #   and that is unlikely to ever change due to their design).
+  #
+  # `FiberExecutor` specific details for submission:
+  #
+  # For `FiberExecutor` the tasks are run immediately upon submission
+  # using the `Fiber.schedule` method.
+  # This method will return
+  # as soon as the Fiber hits a blocking operation
+  # or runs the `Fiber` to completion.
+  # Thus it is completely possible
+  # that the returned `Future` is already completed
+  # by the time it is returned to the caller.
+  #
+  # The `FiberExecutor` implementation does _not_ guarantee
+  # that any particular task will be run concurrently
+  # with any other particular task;
+  # that is dependent
+  # on whether the submitted procs/blocks
+  # have blocking operations that yield control
+  # back to the `Fiber::Scheduler`
+  # and whether the `Fiber::Scheduler` properly implements
+  # `Fiber` switching for those operations.
+  class FiberExecutor
+    include Executor
+
+    # Create a new `FiberExecutor`.
+    #
+    # Spawns fibers via `Fiber.schedule`.
+    #
+    # Raises `AsyncFutures::Error`
+    # unless `Fiber.scheduler` is set.
+    #
+    # Because auto-fibers do not yield control
+    # unless they encounter a blocking operation,
+    # it is completely possible
+    # that the `Fiber` runs to completion upon submission.
+    # Thus, `submit_concurrent` fails by default
+    # unless the parameter `treat_as_concurrent` is set to `true`.
+    #
+    # All internal state is protected via mutex,
+    # so it is safe to use a single `FiberExecutor` instance across multiple threads.
+    # However each thread must have its own `Fiber::Scheduler` set
+    # in order to successfully call `submit`.
+    def initialize(treat_as_concurrent: false)
+      raise Error.new('No Fiber.scheduler set') unless Fiber.scheduler
+
+      super()
+      @treat_as_concurrent = treat_as_concurrent
+      @is_shutdown = false
+      @futures = Set.new
+      @mutex = Thread::Mutex.new
+
+      at_exit { shutdown(wait: false) }
+    end
+
+    # Asynchronously submit a task for execution.
+    #
+    # See `AsyncFutures::Executor.submit` method for full documentation.
+    def submit(*args, **kwargs, &block)
+      raise ArgumentError.new('No block given') unless block
+      raise 'FiberExecutor instance is shutdown' if @mutex.synchronize { @is_shutdown }
+
+      Future.new.tap do |future|
+        @mutex.synchronize { @futures.add future }
+
+        Fiber.schedule do
+          break unless future.set_running_or_notify_cancel
+
+          begin
+            result = block.call(*args, **kwargs)
+          rescue Exception => e # rubocop:disable Lint/RescueException
+            future.set_exception(e)
+          else
+            future.set_result(result)
+          end
+        ensure
+          @mutex.synchronize { @futures.delete future }
+        end
+      end
+    end
+
+    # Raises `NoConcurrencyError`
+    # unless `treat_as_concurrent` was passed as `true`
+    # to the `FiberExecutor` constructor.
+    #
+    # Otherwise,
+    # forwards to `AsyncFutures::FiberExecutor.submit` method.
+    #
+    # See `AsyncFutures::FiberExecutor.submit` method for full documentation.
+    def submit_concurrent(...)
+      raise NoConcurrencyError unless @treat_as_concurrent
+
+      submit(...)
+    end
+
+    # Shutdown `FiberExecutor` instance.
+    #
+    # See `AsyncFutures::Executor.shutdown` for full documentation.
+    def shutdown(wait: true, cancel_futures: false, &block) # rubocop:disable Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
+      block&.call(self)
+    ensure
+      unless check_and_set_shutdown!
+        futures_dup = @mutex.synchronize { @futures.dup } if wait || cancel_futures
+        futures_dup.reject!(&:cancel) if cancel_futures
+        futures_dup.reject!(&:join) if wait
+        @mutex.synchronize { @futures.replace(@futures & futures_dup) } if wait || cancel_futures
+      end
+    end
+
+    private
+
+    # Returns the current shutdown state,
+    # then sets internal shutdown state to `true`.
+    # This is all done atomically to avoid race conditions.
+    def check_and_set_shutdown!
+      @mutex.synchronize do
+        return true if @is_shutdown
+
+        @is_shutdown = true
+        return false
+      end
+    end
+  end
+end
---
+++ test/async_futures/test_fiber_executor.rb
@@ -0,0 +1,137 @@
+# frozen_string_literal: true
+
+require_relative 'minitest_helper'
+
+require 'async'
+require 'async_futures/fiber_executor'
+
+class TestFiberExecutor < Minitest::Test
+  def setup
+    @scheduler = Async::Scheduler.new
+    Fiber.set_scheduler @scheduler
+    @executor = AsyncFutures::FiberExecutor.new
+  end
+
+  def teardown
+    @executor.shutdown(wait: true, cancel_futures: false)
+    Fiber.set_scheduler nil
+  end
+
+  def test_submit_raises_argument_error_without_block
+    assert_raises(ArgumentError) { @executor.submit('No block given') }
+  end
+
+  def test_submit_returns_a_future_object
+    future1 = @executor.submit(1, 2, 3, 4, tell_me: 'that you love me more') do |*args, **kwargs|
+      [args, kwargs]
+    end
+
+    assert_instance_of AsyncFutures::Future, future1
+
+    result = future1.result
+
+    assert_predicate future1, :done?
+    assert_equal 2, result.size
+    assert_instance_of Array, result
+    assert_instance_of Array, result[0]
+    assert_instance_of Hash, result[1]
+  end
+
+  def test_submit_raises_returns_exceptional_future
+    future1 = @executor.submit(1, 2, 3, 4, tell_me: 'that you love me more') do |*args, **kwargs|
+      raise "Some runtime error #{args} #{kwargs}"
+    end
+
+    assert_instance_of AsyncFutures::Future, future1
+    refute_nil future1.exception
+    assert_predicate future1, :done?
+  end
+
+  def test_submit_concurrent_is_alias
+    skip 'not an alias for FiberExecutor'
+
+    assert_equal @executor.method(:submit), @executor.method(:submit_concurrent)
+  end
+
+  def test_map
+    enum = [1, 2, 3, 4]
+    map_result = @executor.map(enum, &:to_s)
+
+    assert_instance_of Enumerator::Lazy, map_result
+
+    results = map_result.to_a
+    first = results.to_a[0]
+    last = results.to_a[3]
+
+    assert_instance_of String, first
+    assert_equal '1', first
+    assert_equal '4', last
+  end
+
+  def test_shutdown_without_block
+    assert_nil @executor.shutdown
+  end
+
+  def test_shutdown_with_block
+    refute_nil(@executor.shutdown { true })
+  end
+
+  def test_submit_after_shutdown
+    @executor.shutdown
+
+    exc = assert_raises(RuntimeError) { @executor.submit { 'hello' } }
+
+    assert_match(/FiberExecutor instance is shutdown/, exc.message)
+  end
+
+  def test_cancel_futures_in_shutdown
+    skip 'currently deadlocking'
+    AsyncFutures::FiberExecutor.new.shutdown do |new_executor|
+      future1 = new_executor.submit { sleep(0.02) }
+      future1.result
+      future2 = new_executor.submit { sleep(0.02) }
+      future3 = new_executor.submit { sleep(0.02) }
+
+      new_executor.shutdown(cancel_futures: true)
+
+      refute_predicate future1, :cancelled?
+
+      # Based on scheduling race conditions,
+      # future2 could be cancelled or not.
+      # We don't know when the worker thread will take control
+      # and pick up another task.
+      # However, because there is only one worker thread,
+      # we know it can't pick up the third submitted task
+      # while it is "working" on the second,
+      # so assuming that the machine running this test isn't dog slow,
+      # we should be able to cancel future3 before the sleep runs out.
+      #
+      # Thus why we only check that future2 is "done?".
+      # We don't know for certain it will be canceled
+      # or if the worker thread will pick it up.
+      assert_predicate future2, :done?
+
+      assert_predicate future3, :cancelled?
+    end
+  end
+
+  def test_cancel_futures_manually # rubocop:disable Metrics/AbcSize
+    skip 'currently deadlocking'
+    AsyncFutures::FiberExecutor.new.shutdown do |new_executor|
+      future1 = new_executor.submit { sleep(0.02) }
+      future2 = new_executor.submit { sleep(0.02) }
+      future3 = new_executor.submit { sleep(0.02) }
+
+      assert_predicate future2, :pending?
+      assert_predicate future3, :pending?
+
+      future2.cancel
+      future3.cancel
+      future1.result
+
+      refute_predicate future1, :cancelled?
+      assert_predicate future2, :cancelled?
+      assert_predicate future3, :cancelled?
+    end
+  end
+end
