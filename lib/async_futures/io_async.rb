74b1ba9349008dae4825ca7a293e1d372b83486b
Ethan Estrada <ethan@misterfidget.com>
2026-07-08 23:14:54 -0600

    Begin IO Async example work
    Tests yet not completed.
    
    WIP for issue #2

---
+++ lib/async_futures/io_async.rb
@@ -0,0 +1,153 @@
+# frozen_string_literal: true
+
+require_relative 'future'
+
+require 'set' # rubocop:disable Lint/RedundantRequireStatement
+
+module AsyncFutures
+  # Simple example mixin for async IO.
+  #
+  # All reads and writes are done on a single background worker thread.
+  #
+  # This is not the most efficient implementation.
+  # It is just meant to be an example
+  # of how one can use the `Future` class
+  # outside of an `Executor` implementation.
+  module IOAsync
+    # Return an incomplete future
+    # that will eventually contain an integer with the number of bytes written
+    # or an exception if the string could not be written for some reason.
+    #
+    # The string is written in a nonblocking fashion
+    # on a background worker thread.
+    #
+    # If the process shuts down before the future can be completed,
+    # the work will be abandoned even if it partially completed.
+    def write_async(string) # rubocop:disable Metrics/AbcSize
+      Future.new.tap do |ftr|
+        maybe_spawn_worker
+
+        io_async_queue.push(proc {
+          break ftr unless ftr.set_running_or_notify_cancel
+
+          all_written = 0
+
+          loop do
+            bytes_written = write_nonblock(string)
+            string = string[bytes_written..nil]
+            all_written += bytes_written
+            break ftr.tap { ftr.set_result(all_written) } if string.empty?
+          rescue IO::WaitReadable, IO::WaitWritable, Errno::EINTR
+            Fiber.yield nil
+            retry
+          rescue Exception => e # rubocop:disable Lint/RescueException
+            break ftr.tap { ftr.set_exception(e) }
+          end
+        })
+      end
+    end
+
+    # Return an incomplete future
+    # that will eventually contain the string value read from the IO object
+    # or an exception if the IO object could not be read from for some reason.
+    #
+    # The string is read in a nonblocking fashion
+    # on a background worker thread.
+    #
+    # If the process shuts down before the future can be completed,
+    # the work will be abandoned even if it partially completed.
+    def read_async(maxlen = nil) # rubocop:disable Metrics/AbcSize
+      Future.new.tap do |ftr|
+        maybe_spawn_worker
+
+        io_async_queue.push(proc {
+          break ftr unless ftr.set_running_or_notify_cancel
+
+          to_read_length = maxlen
+          retrieved_str = String.new
+
+          loop do
+            retrieved_str << read_nonblock(to_read_length)
+            to_read_length = maxlen - retrieved_str.size unless maxlen.nil?
+            break ftr.tap { ftr.set_result(retrieved_str) } unless maxlen.nil? || to_read_length.positive?
+          rescue IO::WaitReadable, IO::WaitWritable, Errno::EINTR
+            Fiber.yield nil
+            retry
+          rescue EOFError
+            break ftr.tap { ftr.set_result(retrieved_str) }
+          rescue Exception => e # rubocop:disable Lint/RescueException
+            break ftr.tap { ftr.set_exception(e) }
+          end
+        })
+      end
+    end
+
+    private
+
+    def maybe_spawn_worker
+      Ractor.store_if_absent(:io_async_worker) do
+        Thread.new do
+          worker_fibers = Set.new
+          while (fproc = io_async_queue.pop(timeout: 0))
+            # Ignore all remaining work once the queue is closed.
+            break if io_async_queue.closed?
+
+            worker_fibers.add(Fiber.new(&fproc)) unless fproc.nil?
+
+            # sleep for 10ms
+            # only if all the workers return `nil`
+            # (i.e. none completed their future)
+            sleep(0.01) unless worker_fibers.reject!(&:resume)
+          end
+        end
+      end
+    end
+
+    def io_async_queue
+      Ractor.store_if_absent(:io_async_queue) do
+        Thread::Queue.new.tap { |q| at_exit { q.close } }
+      end
+    end
+  end
+
+  # Simple mixin for sync IO with an async interface.
+  module IOSync
+    # Return a completed future
+    # containing an integer with the number of bytes written.
+    #
+    # This exists for compatibility
+    # with classes with true nonblocking reading methods.
+    # There is no performance benefit
+    # to calling this instead of directly calling `write`.
+    def write_async(string)
+      Future.new.tap do |ftr|
+        ftr.complete(string, &method(:write))
+      end
+    end
+
+    # Return a completed future
+    # containing a string up to `maxlen` bytes long.
+    #
+    # This exists for compatibility
+    # with classes with true nonblocking reading methods.
+    # There is no performance benefit
+    # to calling this instead of directly calling `read`.
+    def read_async(maxlen = nil)
+      Future.new.tap do |ftr|
+        ftr.complete(maxlen, &method(:read))
+      end
+    end
+  end
+end
+
+class ::IO # rubocop:disable Style/OneClassPerFile
+  include ::AsyncFutures::IOAsync
+end
+
+class ::OpenSSL::SSL::SSLSocket # rubocop:disable Style/OneClassPerFile,Style/ClassAndModuleChildren
+  include ::AsyncFutures::IOAsync
+end
+
+class ::StringIO # rubocop:disable Style/OneClassPerFile
+  include ::AsyncFutures::IOSync
+end
